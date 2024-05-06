e4ab45c230c19bd06904770872d3d885bfc845ae
Faq89 <facundoledesma89@gmail.com>
2024-05-06 02:07:30 -0300

    Presentación
    se presenta nuevamente corrigiendo errores en como se presento

--- practicos/06 - 59093 - Ledesma, Facundo Osvaldo/TP1/index.jsx
+++ practicos/06 - 59093 - Ledesma, Facundo Osvaldo/TP1/index.jsx
@@ -1,8 +1,36 @@
-const App = () =>  (
-    <div>
-        <h1>¡Hola, mundo!</h1>
-        <p>¡Bienvenidos a React!</p>
-    </div>
-)
+const contactos = [
+  { id: 1, nombre: "Rosario", apellido: "Sanchez", telefono: "381477787" },
+  { id: 2, nombre: "Santiago", apellido: "Salazar", telefono: "381529918" },
+  { id: 3, nombre: "Miguel", apellido: "Valdez", telefono: "381419283" },
+  { id: 3, nombre: "Facundo", apellido: "Ledesma", telefono: "381346781" },
+];
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const Contacto = ({ contacto }) => (
+  <div class="boxesContainer">
+    <div class="cardBox">
+      <div class="card">
+        <div class="front">
+          <p>ID: {contacto.id}</p>
+          <p>Nombre: {contacto.nombre} </p>
+          <p>Apellido: {contacto.apellido}</p>
+        </div>
+        <div class="back">
+          <p>Teléfono: {contacto.telefono}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+const Agenda = ({ contactos }) => (
+  <div className="TablaTarjetas">
+    {contactos.map((c) => (
+      <Contacto key={c.id} contacto={c} />
+    ))}
+  </div>
+);
+
+ReactDOM.render(
+  <Agenda contactos={contactos} />,
+  document.getElementById("root")
+);
