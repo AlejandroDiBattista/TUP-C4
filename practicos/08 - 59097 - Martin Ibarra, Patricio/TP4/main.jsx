083d98b57f2b71ff3824b323115cb3bbc976ce10
patricio <patriciomartinibarra@gmail.com>
2024-06-03 03:25:01 -0300

    commit

--- practicos/08 - 59097 - Martin Ibarra, Patricio/TP4/main.jsx
+++ practicos/08 - 59097 - Martin Ibarra, Patricio/TP4/main.jsx
@@ -1,7 +1,15 @@
-
 const API_KEY = '30d38b26954359266708f92e1317dac0'
+
+const UrlApi = "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";
+
 function App() {
     return <>
-        <h1>App Clima</h1>
+        <h1>Clima</h1>
+        <div className="ciudades">
+            <a href="#" id="tucuman">Tucumán</a>
+            <a href="#" id="salta">Salta</a>
+            <a href="#" id="buenosaires">Buenos Aires</a>     
+        </div> 
+        <input type="text" id="buscador"className="buscador" placeholder="Buscar ciudad..."></input>   
     </>
 }
