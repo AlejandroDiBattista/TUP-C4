e0105f3bda7db5ab152cff313369f94439aa5b1c
Stefano Ferrari <stefanoferrari.dev@gmail.com>
2024-06-04 15:37:51 -0300

    Presentación TP5 - Stefano Ferrari

--- practicos/03 - 50694 - Ferrari, Stefano/TP5/app.js
+++ practicos/03 - 50694 - Ferrari, Stefano/TP5/app.js
@@ -1,21 +1,112 @@
-import express from 'express'
-import cors from 'cors'
+import express from "express";
+import cors from "cors";
 
-const app = express()
+const app = express();
 
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
 let datos = [
-    // Datos de ejemplo   
-]
+  {
+    id: 1,
+    nombre: "Michael",
+    apellido: "Scott",
+    edad: 45,
+    borrado: false,
+    actualizado: Date.now(),
+  },
+  {
+    id: 2,
+    nombre: "Dwight",
+    apellido: "Schrute",
+    edad: 35,
+    borrado: false,
+    actualizado: Date.now(),
+  },
+  {
+    id: 3,
+    nombre: "Jim",
+    apellido: "Halpert",
+    edad: 32,
+    borrado: false,
+    actualizado: Date.now(),
+  },
+  {
+    id: 4,
+    nombre: "Pam",
+    apellido: "Beesly",
+    edad: 31,
+    borrado: false,
+    actualizado: Date.now(),
+  },
+  {
+    id: 5,
+    nombre: "Stanley",
+    apellido: "Hudson",
+    edad: 58,
+    borrado: false,
+    actualizado: Date.now(),
+  },
+  {
+    id: 6,
+    nombre: "Angela",
+    apellido: "Martin",
+    edad: 39,
+    borrado: false,
+    actualizado: Date.now(),
+  },
+];
 
-app.get('/personas', (req, res) => {
-    // Implementar GET_ALL
+function validarDatosPersona(persona) {
+  if (!persona.nombre || !persona.apellido || !persona.edad) {
+    throw new Error("Faltan campos obligatorios");
+  }
+}
+
+app.get("/personas", (req, res) => {
+  const data = datos.filter((persona) => !persona.borrado);
+  res.status(200).json(data);
 });
 
-app.put('/personas', (req, res) => {
-    // Implementar PUT
-})
+app.put("/personas", (req, res) => {
+  try {
+    const { id, nombre, apellido, edad, borrado } = req.body;
 
-export default app
+    if (id === undefined) {
+      const nuevaPersona = {
+        id: datos.length + 1,
+        nombre,
+        apellido,
+        edad,
+        borrado: borrado || false,
+        actualizado: Date.now(),
+      };
+
+      validarDatosPersona(nuevaPersona);
+      datos.push(nuevaPersona);
+      return res.status(201).json(nuevaPersona);
+    } else {
+      const indexPersona = datos.findIndex((persona) => persona.id === id);
+
+      if (indexPersona === -1) {
+        return res.status(404).json({ error: "Persona no encontrada" });
+      }
+
+      datos[indexPersona] = {
+        ...datos[indexPersona],
+        ...req.body,
+        actualizado: Date.now(),
+      };
+
+      if (borrado) {
+        datos[indexPersona].borrado = true;
+      }
+
+      res.status(201).json(datos[indexPersona]);
+    }
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+export default app;
