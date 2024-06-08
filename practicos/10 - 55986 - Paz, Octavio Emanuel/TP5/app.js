9409e06050799aae6fec4487e3933919636e1f2e
Octavio Paz <octavioemanuelpaz@gmail.com>
2024-06-07 23:59:29 -0300

    tp5 Paz, Octavio
    presentación trabajo practico N°5

--- practicos/10 - 55986 - Paz, Octavio Emanuel/TP5/app.js
+++ practicos/10 - 55986 - Paz, Octavio Emanuel/TP5/app.js
@@ -1,21 +1,57 @@
-import express from 'express'
-import cors from 'cors'
+import express from 'express';
+import cors from 'cors';
 
-const app = express()
+const app = express();
 
+//middlewares
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
 let datos = [
-    // Datos de ejemplo   
-]
+    { id: 1, nombre: 'Cecilio', apellido: 'Rodriguez', edad: 18, borrado: false, actualizado: Date.now() },
+    { id: 2, nombre: 'Joaquin', apellido: 'Pereyra', edad: 25, borrado: false, actualizado: Date.now() },
+    { id: 3, nombre: 'Juan', apellido: 'Musso', edad: 30, borrado: false, actualizado: Date.now() },
+    { id: 4, nombre: 'Erling', apellido: 'Halland', edad: 35, borrado: false, actualizado: Date.now() },
+    { id: 5, nombre: 'Elon', apellido: 'Musk', edad: 45, borrado: false, actualizado: Date.now() },
+    { id: 6, nombre: 'Lionel', apellido: 'Messi', edad: 36, borrado: false, actualizado: Date.now() },
+    { id: 7, nombre: "Maria", apellido: "Roldan", edad: 50, borrado: true, actualizado: Date.now() },
+    { id: 8, nombre: "Candelaria", apellido: "Caceres", edad: 21, borrado: false, actualizado: Date.now() },
+    { id: 9, nombre: "Sergio", apellido: "Aguero", edad: 29, borrado: true, actualizado: Date.now() }
+];
 
+// GET_ALL: Obtener todas las personas
 app.get('/personas', (req, res) => {
-    // Implementar GET_ALL
+    const noBorrados = datos.filter(persona => !persona.borrado);
+    res.status(200).json(noBorrados);
 });
 
+// SET: Modificar o crear una persona
 app.put('/personas', (req, res) => {
-    // Implementar PUT
-})
+    const persona = req.body;
 
-export default app
+    if (persona.id === undefined) {
+        const nuevaPersona = {
+            ...persona,
+            id: datos.length ? Math.max(...datos.map(persona => persona.id)) + 1 : 1,
+            borrado: false,
+            actualizado: Date.now()
+        };
+        datos.push(nuevaPersona);
+        res.status(201).json({ id: nuevaPersona.id });
+    } else {
+        const index = datos.findIndex(p => p.id === persona.id);
+        if (index === -1) {
+            res.status(404).send();
+        } else {
+            const personaActualizada = {
+                ...datos[index],
+                ...persona,
+                actualizado: Date.now()
+            };
+            datos[index] = personaActualizada;
+            res.status(201).json(personaActualizada);
+        }
+    }
+});
+
+export default app;
