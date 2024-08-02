33855e36df43ed865d893e7fffbb8f026d3b9f14
Alejandro Di Battista <alejandrodibattista@gmail.com>
2024-08-02 02:12:11 -0300

    .

--- actualizar.js
+++ actualizar.js
@@ -1,20 +1,11 @@
-
-function subirCondicionC4() {
+function cambiarCondicionC4() {
     let datos = [1, 4, 4, 4, 4, 4, 1, 1, 4, 4, 2, 4, 4, 4, 4, 2, 4, 1]
     let estados = document.querySelectorAll('select[name="nota"]');
-    if (datos.length != estados.length) {
-        alert('Error en la cantidad de alumnos >> Alumnos: ' + datos.length + 'Condiciones: ' + estados.length);
-    } else {
-        estados.forEach((estado, index) => estado.selectedIndex = datos[index]);
-    }
+    estados.forEach((estado, index) => estado.selectedIndex = datos[index]);
 }
 
-function subirNotaC4() {
-    let datos = [1, 10, 10, 10, 9, 10, 1, 1, 10, 10, 6, 9, 10, 9, 10, 8, 10, 1]
+function cambiarNotaC4() {
+    let datos = [0, 10, 10, 10, 9, 10, 0, 0, 10, 10, 0, 9, 10, 9, 10, 0, 10, 0]
     let notas = document.querySelectorAll('input[name="nota"]');
-    if (datos.length != notas.length) {
-        alert('Error en la cantidad de alumnos >> Alumnos: ' + datos.length + 'Notas: ' + notas.length);
-    } else {
-        notas.forEach((nota, index) => nota.value = datos[index]);
-    }
+    notas.forEach((nota, index) => nota.value = datos[index]);
 }
